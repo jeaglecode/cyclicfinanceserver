dc831b22efa886b36d9899ce7b6e4d957cb4a798
jeaglecode <32748770+jeaglecode@users.noreply.github.com>
2023-05-10 20:25:37 -0700

    without cors

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+// import cors from "cors";
 const express = require('express');
 const fs = require('fs');
 const app = express();
-const cors = require('cors');
+// const cors = require('cors');
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
 app.all('/', (req, res) => {
     console.log('Just got a request!');
